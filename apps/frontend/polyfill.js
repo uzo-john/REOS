2328db21d67f038354939e5a368c7cf7ceaf9e4e
uzo-john <johnpauluzowuru2018@gmail.com>
2026-07-11 23:32:48 +0100

    fix(frontend): remove Node-only util.styleText polyfill from frontend

--- apps/frontend/polyfill.js
+++ apps/frontend/polyfill.js
@@ -1,35 +1,3 @@
-const util = require('util');
-
-if (!util.styleText) {
-  util.styleText = function(style, text) {
-    const styles = {
-      red: '\x1b[31m',
-      green: '\x1b[32m',
-      yellow: '\x1b[33m',
-      blue: '\x1b[34m',
-      magenta: '\x1b[35m',
-      cyan: '\x1b[36m',
-      white: '\x1b[37m',
-      gray: '\x1b[90m',
-      bold: '\x1b[1m',
-      reset: '\x1b[0m'
-    };
-    
-    let start = '';
-    const end = '\x1b[0m';
-    
-    if (Array.isArray(style)) {
-      for (const s of style) {
-        if (styles[s]) start += styles[s];
-      }
-    } else if (styles[style]) {
-      start = styles[style];
-    }
-    
-    return start + text + end;
-  };
-}
-
 if (!Array.prototype.toReversed) {
   Array.prototype.toReversed = function() {
     return [...this].reverse();
